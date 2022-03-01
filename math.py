4dd2921e700dafdbbb32624e5e69a64fae5873c6
UdayalaxmiNagubandi <100360255+UdayalaxmiNagubandi@users.noreply.github.com>
2022-03-01 16:31:10 +0530

    initial commit on math.py

---
+++ math.py
@@ -0,0 +1,17 @@
+#add
+def add(x,y):
+    return x+y
+#sub   
+def sub(x,y):
+    return x-y #on master branch
+#mul    
+def mul(x,y):
+    return x*y #on bug456 branch
+#div   
+def divd(x,y):
+    if(y==0):
+        return ERROR
+    else:
+        return x/y
+
+    
